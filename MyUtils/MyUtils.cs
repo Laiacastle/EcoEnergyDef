71ac98bab6c920232203e93bb507209d5431658b
Laiacastle <laia.castillo.7e6@itb.cat>
2025-01-23 19:53:22 +0100

    error

--- MyUtils/MyUtils.cs
+++ MyUtils/MyUtils.cs
@@ -51,7 +51,7 @@ namespace MyUtils
         /// </summary>
         /// <param>int i 1, 2 depén de la posició del més gran</param>
         /// <returns>retorna int</returns>
-        public static int ComprovarNum(int num2, int compara) 
+        public static int ComprovarNum(int num2, int compara)
         {
             const string Msg = "\t\t\t\tEl format no es correcte",
                 MsgInp = "\t\t> ";
@@ -67,18 +67,18 @@ namespace MyUtils
             {
                 result = Convert.ToInt32(num);
             }
-            
+
             switch (compara)
             {
                 case 1:
-                    while (result < num2 || result > 99) 
+                    while (result < num2 || result > 99)
                     {
                         CambiarColor(Msg, "red");
                         Console.Write(MsgInp);
                         result = Convert.ToInt32(Console.ReadLine());
-                    };break;
+                    }; break;
                 case 2:
-                    while (result > num2 || result > 99) 
+                    while (result > num2 || result > 99)
                     {
                         CambiarColor(Msg, "red");
                         Console.Write(MsgInp);
@@ -86,7 +86,7 @@ namespace MyUtils
                     }; break;
             }
             return result;
-            
+
         }
 
         /// <summary>
@@ -135,7 +135,7 @@ namespace MyUtils
         {
             const string MsgTipus = "\t\t\t\tIndica el tipus d'energia (solar, eolica, hidroelectrica)",
             MsgError = "\t\t\t\tEl valor es incorrecte";
-            
+
             Console.WriteLine(MsgTipus);
             Console.Write("\t\t> ");
             string? tipus = Console.ReadLine() ?? "";
@@ -147,39 +147,8 @@ namespace MyUtils
                 tipus = Console.ReadLine() ?? "";
                 tipus = tipus.ToUpper();
             }
-            
+
             if (tipus == "EOLICA") { return 1; } else if (tipus == "HIDROELECTRICA") { return 2; } else { return 3; }
-        }
-
-        //Per cambiar el color al text
-         public enum Color {
-            Red,
-            Blue,
-            Green,
-            Yellow,
-            Black
-        }
-
-        public static Color ObtenirColor(string color) {
-            if (color.ToUpper() == "RED") {
-                return Color.Red;
-            }
-            else if (color.ToUpper() == "YELLOW")
-            {
-                return Color.Yellow;
-            }
-            else if (color.ToUpper() == "BLUE")
-            {
-                return Color.Blue;
-            }
-            else if (color.ToUpper() == "GREEN")
-            {
-                return Color.Green;
-            }
-            else
-            {
-                return Color.Black;
-            }
         }
 
         /// <summary>
@@ -189,15 +158,27 @@ namespace MyUtils
         /// <returns>no retorna res</returns>
         public static void CambiarColor(string text, string color)
         {
-            switch (ObtenirColor(color))
+            if (color.ToUpper() == "RED")
             {
-                case Color.Red:Console.ForegroundColor = ConsoleColor.DarkRed;break;
-                case Color.Blue: Console.ForegroundColor = ConsoleColor.DarkBlue;break;
-                case Color.Yellow: Console.ForegroundColor = ConsoleColor.DarkYellow;break;
-                case Color.Green: Console.ForegroundColor= ConsoleColor.DarkGreen;break;
-                case Color.Black: Console.ForegroundColor = ConsoleColor.Black;break;
+                Console.ForegroundColor = ConsoleColor.DarkRed;
             }
-            
+            else if (color.ToUpper() == "YELLOW")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+            else if (color.ToUpper() == "BLUE")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            }
+            else if (color.ToUpper() == "GREEN")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
             Console.WriteLine(text);
             Console.ForegroundColor = ConsoleColor.White;
         }
